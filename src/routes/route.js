945fc965543da75336db9eed937563a7b9a1b49f
amarjeetdeveloper <102247009+amarjeetdeveloper@users.noreply.github.com>
2022-04-09 18:57:20 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -61,6 +61,56 @@ router.post('/test-post2', function (req, res) {
 });
 
 
+
+const express = require('express');
+const router = express.Router();
+ 
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+ 
+   router.post('/players', function (req, res) {
+     let data = req.body;
+     console.log(data)
+   
+     players.filter((x)=> {if(x.name === data.name){
+       return res.send({msg : "data already exist"})}
+   })
+  players.push(data);
+  res.send(players);
+    });
+  
+
+module.exports = router;
+   
+
 const randomController= require("../controllers/randomController.js")
 //write a post request to accept an element in post request body and add it to the given array and return the new array
 router.post('/test-post3', randomController.addToArray ); //HANDLER/CONTROLLER
