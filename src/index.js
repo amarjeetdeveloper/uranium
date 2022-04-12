c153a1f232664756068bb25b136098a164c2ca20
poonam sandip patel <sarbhadi@gmail.com>
2022-04-12 08:36:04 +0530

    mongodb first assignment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://raj:lHhCcZCKtw7us6Xs@cluster0.k6wio.mongodb.net/raj", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
