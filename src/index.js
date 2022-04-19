b7e36b8e87e0f1ecbba1a9c443be605c2a4ae1de
poonam sandip patel <sarbhadi@gmail.com>
2022-04-20 00:22:02 +0530

    middleware first attempt

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://amarjeet:I96n7yPNgpjFi1h7@cluster0.fkovg.mongodb.net/amarjeet", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -16,7 +16,8 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 
 app.use (
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        const date = Date();
+        console.log(date +   req.ip   + req.path)
         next();
   }
   );
--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -1,22 +1,22 @@
 
 const mid1= function ( req, res, next) {
-    req.falana= "hi there. i am adding something new to the req object"
-    console.log("Hi I am a middleware named Mid1")
+    const date = new Date();
+    console.log(date + req.ip + req.url)
     next()
 }
 
 const mid2= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid2")
+    console.log(date + req.ip + req.url)
     next()
 }
 
 const mid3= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid3")
+    console.log(date + req.ip + req.url)
     next()
 }
 
 const mid4= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid4")
+    console.log(date + req.ip + req.url)
     next()
 }
 
